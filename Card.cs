ec14234789090ef64649d0709f8598b6af40c07f
MinnyS <minsullivan@sullivandesigns.us>
2023-07-08 12:27:17 -0400

    feat: add card classes so card objects can be created and random cards can be picked

---
+++ Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PickRandomCards
+{
+	public class Card
+	{
+		// Properties
+		public readonly CardSuit Suit;
+		public readonly CardRank Rank;
+
+		// Constructor for specifying suit and rank
+		public Card(CardSuit suit, CardRank rank)
+		{
+			Suit = suit;
+			Rank = rank;
+		}
+
+		// Constructor for picking random Suit and Rank
+		public Card()
+		{
+			Suit = CardPicker.RandomSuit();
+			Rank = CardPicker.RandomRank();
+		}
+
+		// ToString method
+        public override string ToString()
+        {
+			return Rank + " of " + Suit;
+        }
+    }
+}
+
